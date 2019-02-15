3928b0d3e8e5de63f45300b9ca58ff75d00a52b8
willmakenoise <marius@stedjan.com>
2019-02-15 18:37:30 +0100

    Scaffold

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "website_sale_clear_search",
+
+    'summary': """
+        Clear Search when switching categories""",
+
+    'description': """
+        Clear Search when switching categories
+    """,
+
+    'author': "Marius Stedjan - Nordoo",
+    'website': "http://www.nordoo.no",
+    'category': 'ecommerce',
+    'version': '0.1',
+    'depends': ['website_sale'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        # 'views/templates.xml',
+    ],
+}
